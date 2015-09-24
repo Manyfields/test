c07e671069396745e76dbc3da79bf07c1abfe947
Manyfields <deschampsmaxence93@gmail.com>
2015-09-24 12:24:50 -0400

    Third thingy
    Added button and broken code

--- DemoGit/app/src/main/java/com/md1123721/oracletech/jac/demogit/MainActivity.java
+++ DemoGit/app/src/main/java/com/md1123721/oracletech/jac/demogit/MainActivity.java
@@ -7,10 +7,14 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    private Button pressMeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pressMeButton = (pressMeButton) 
     }
 
     @Override
